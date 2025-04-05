5c1682eb3743a48ad1aa71b7861c2d77fa80bd27
paulokruk <73293067+paulokruk@users.noreply.github.com>
2025-04-05 10:26:54 -0300

    Update 1_🏡_Home.py

--- 1_🏡_Home.py
+++ 1_🏡_Home.py
@@ -14,10 +14,7 @@ if 'data' not in st.session_state:
 st.markdown('# FIFA23 OFFICIAL DATASET ⚽')
 st.sidebar.markdown('Desenvolvido por [Kruk]')
 
-btn = st.button('Acesse os dados no Kaggle')
-if btn:
-    webbrowser.open_new_tab("https://www.kaggle.com/datasets/kevwesophia/fifa23-official-datasetclean-data")
-
+btn = st.link_button('Acesse os dados no Kaggle', "https://www.kaggle.com/datasets/kevwesophia/fifa23-official-datasetclean-data")
 
 st.markdown(
     """
